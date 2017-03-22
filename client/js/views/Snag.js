a2c89642ecbe2d4b7e0270826b4978ef795f3054
ScottAP108 <saparton@gmail.com>
2017-03-22 14:51:37 -0400

    styling, swipe left to cycle through spots

--- client/js/views/Snag.js
+++ client/js/views/Snag.js
@@ -1,5 +1,7 @@
 module.exports = Object.assign( {}, require('./__proto__'), {
 
+    garagePath: require('./lib/carPath'),
+
     bindSwipe() {
         this.swipeTime = 1000
         this.minX = 30
@@ -17,10 +19,11 @@ module.exports = Object.assign( {}, require('./__proto__'), {
 
     data: [
         {
+            id: 1,
             distance: '300 ft away',
             mapLocation: { lat: -34.397, lng: 150.644 },
             spotLocation: { lat: -34.397, lng: 150.644 },
-            name: 'LAZParking',
+            name: 'LAZ Parking',
             context: [
                 'Private Parking Structure',
                 '88/240 spots'
@@ -30,13 +33,14 @@ module.exports = Object.assign( {}, require('./__proto__'), {
                 '4hr max',
                 'open 24 hrs'
             ],
-            image: 'trump.jpg',
+            image: 'sidekick-shed-xs.jpg',
             temp: 57
         },
         {
+            id: 2,
             distance: '2 mi',
             mapLocation: { lat: 40.004, lng: -75.258 },
-            spotLocation: { lat: 39.9388, lng: -751.1832 },
+            spotLocation: { lat: 40.004, lng: -75.258 },
             name: 'Philly',
             context: [
                 'Private Parking Structure',
@@ -51,26 +55,26 @@ module.exports = Object.assign( {}, require('./__proto__'), {
             temp: 10
         },
         {
-            distance: '2 mi',
-            mapLocation: { lat: 40.004, lng: -75.258 },
-            spotLocation: { lat: 39.9388, lng: -751.1832 },
-            name: 'Philly',
+            id: 3,
+            distance: '3 mi',
+            mapLocation: { lat: 40, lng: -80 },
+            spotLocation: { lat: 40, lng: -80 },
+            name: 'Not Philly',
             context: [
-                'Private Parking Structure',
-                '88/240 spots'
+                'Another Private Parking Structure',
+                '100/240 spots'
             ],
             details: [
-                '$3/hr',
+                '$4/hr',
                 '4hr max',
                 'open 24 hrs'
             ],
-            image: 'trump.jpg',
-            temp: 10
+            image: 'sidekick-shed-xs.jpg',
+            temp: 30
         }
     ],
 
     initMap() {
-
         this.map = new google.maps.Map( this.els.map, {
           center: this.data[ this.currentSpot ].mapLocation,
           disableDefaultUI: true,
@@ -78,38 +82,59 @@ module.exports = Object.assign( {}, require('./__proto__'), {
           zoom: 14
         } )
 
+        this.icon = {            
+            path: this.garagePath,
+            strokeColor: '#d380d6',
+            fillOpacity: 1,
+            anchor: new google.maps.Point(0,0),
+            strokeWeight: 1,
+            scale: .050
+        }
+
+        this.renderIcons()
+
         this.update()
-        this.els.temp.textContent = this.data[0].temp
-        this.els.spotName.textContent = this.data[0].name
-        this.els.spotContext.innerHTML = this.data[0].context.join( this.templates.Dot )
-        this.els.detail.innerHTML = this.data[0].details.join( this.templates.Dot )
-        this.els.image.src = `/static/img/${this.data[0].image}`
+
+    },
+
+    renderIcons() {
+        this.data.forEach( datum =>
+            this.markers[ datum.id ] = new google.maps.Marker( {
+                position: datum.spotLocation,
+                map: this.map,
+                title: datum.name,
+                icon: this.icon
+            } )
+        )
     },
 
     onSwipeStart( e ) {
-      e = ('changedTouches' in e) ? e.changedTouches[0] : e
-      this.touchStartCoords = { x: e.pageX, y :e.pageY }
-      this.startTime = new Date().getTime()
+        e = ('changedTouches' in e) ? e.changedTouches[0] : e
+        this.touchStartCoords = { x: e.pageX, y :e.pageY }
+        this.startTime = new Date().getTime()
     },
     
     onSwipeMove( e ) {
+        //console.log( 'onSwipeMove' )
         e.preventDefault()
     },
     
     onSwipeEnd( e ) {
-      e = ('changedTouches' in e) ? e.changedTouches[0] : e
-      this.touchEndCoords = { x: e.pageX - this.touchStartCoords.x, y :e.pageY - this.touchStartCoords.y }
-      this.elapsedTime = new Date().getTime() - this.startTime
+        e = ('changedTouches' in e) ? e.changedTouches[0] : e
+        this.touchEndCoords = { x: e.pageX - this.touchStartCoords.x, y :e.pageY - this.touchStartCoords.y }
+        this.elapsedTime = new Date().getTime() - this.startTime
 
-      if( this.elapsedTime <= this.swipeTime ) {
-        if( Math.abs(this.touchEndCoords.x) >= this.minX && Math.abs(this.touchEndCoords.y) <= this.maxY ) {
-          this.touchEndCoords.x < 0 ? this.onSwipeLeft() : this.onSwipeRight()
-          }
+        if( this.elapsedTime <= this.swipeTime ) {
+            if( Math.abs(this.touchEndCoords.x) >= this.minX && Math.abs(this.touchEndCoords.y) <= this.maxY ) {
+                this.touchEndCoords.x < 0 ? this.onSwipeLeft() : this.onSwipeRight()
+            }
         }
     },
 
     onSwipeLeft() {
-        console.log('left')
+        this.currentSpot = this.data[ this.currentSpot + 1 ] ? this.currentSpot + 1 : 0
+
+        this.update()
     },
     
     onSwipeRight() {
@@ -118,13 +143,13 @@ module.exports = Object.assign( {}, require('./__proto__'), {
 
     postRender() {
         this.currentSpot = 0
+        this.markers = { }
 
         window.google
             ? this.initMap()
             : window.initMap = this.initMap
 
-        this.bindSwipe()
-        
+        this.bindSwipe()        
         
         return this
     },
@@ -139,6 +164,7 @@ module.exports = Object.assign( {}, require('./__proto__'), {
         this.map.setCenter( data.mapLocation )
         this.els.temp.textContent = data.temp
         this.els.spotName.textContent = data.name
+        this.els.distance.textContent = data.distance
         this.els.spotContext.innerHTML = data.context.join( this.templates.Dot )
         this.els.detail.innerHTML = data.details.join( this.templates.Dot )
         this.els.image.src = `/static/img/${data.image}`
--- client/js/views/templates/Snag.js
+++ client/js/views/templates/Snag.js
@@ -4,9 +4,15 @@ module.exports = p => {
         <div class="map-wrap">
             <div data-js="map" class="map"></div>
             <div class="menu">
-                <div>${require('./lib/info')}</div> 
-                <div>${require('./lib/cursor')}</div> 
+                <div class="menu-item">
+                    <div>${require('./lib/info')}</div>
+                </div>
+                <div class="menu-item">
+                    <div>${require('./lib/cursor')}</div>
+                </div>
             </div>
+        </div>
+        <div class="info-main">
             <div data-js="pageUi" class="page-ui">
                 ${Array.from(Array(p.length).keys()).map( () => pageUi ).join('')}
             </div>
@@ -14,18 +20,20 @@ module.exports = p => {
                 <span>${require('./lib/cloud')}</span>
                 <span data-js="temp"></span>
             </div>
-        </div>
-        <div class="info">
-            <div class="line-wrap">${require('./lib/line')}</div>
-            <div class="button-row">
-                <button class="snag" data-js="snag">
-                    <span>Snag This Spot</span>
-                    <span data-js="distance"></span>
-                </button>
+            <div class="snag-area">
+                <div class="line-wrap">${require('./lib/line')}</div>
+                <div class="button-row">
+                    <button class="snag" data-js="snag">
+                        <div>Snag This Spot</div>
+                        <div data-js="distance"></div>
+                    </button>
+                </div>
             </div>
-            <div data-js="spotName"></div>
-            <div data-js="spotContext"></div>
-            <div data-js="detail"></div>
-            <img data-js="image" />
+            <div class="spot-details">
+                <div data-js="spotName"></div>
+                <div data-js="spotContext"></div>
+                <div data-js="detail"></div>
+                <img data-js="image" />
+            </div>
         </div>
     </div>` }
