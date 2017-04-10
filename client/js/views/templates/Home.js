0e86855b0474cc595372b0a3f6c5f7891a485e51
Christopher Baron <topherbaron@gmail.com>
2017-04-10 17:13:49 -0400

    fixed Home

--- client/js/views/templates/Home.js
+++ client/js/views/templates/Home.js
@@ -1,1 +1,1 @@
-module.exports = p => `<div><div data-js="logoWrap">${require('./lib/logo')}</div><div>Finding the closest parking spots...sit tight.</div>`
+module.exports = p => `<div><div data-js="logoWrap">${require('./lib/logo')}</div><div class="footer">Finding the closest parking spots...sit tight.</div>`
