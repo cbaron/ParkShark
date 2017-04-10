a1276055032fffb4abf8775c763410d64fafdb1f
Christopher Baron <topherbaron@gmail.com>
2017-04-10 16:57:51 -0400

    fixed Home

--- client/js/views/Home.js
+++ client/js/views/Home.js
@@ -1,6 +1,8 @@
 module.exports = Object.assign( {}, require('./__proto__'), {
 
     postRender() {
+        this.els.logoWrap.style.height = `${window.innerHeight / 2}px`
+
         setTimeout( () => this.hide().then( () => this.emit( 'navigate', 'snag' ) ).catch( this.Error ), 2000 )
         
         return this
--- client/js/views/templates/Home.js
+++ client/js/views/templates/Home.js
@@ -1,1 +1,1 @@
-module.exports = p => `<div><div>${require('./lib/logo')}</div><div>Finding the closest parking spots...sit tight.</div>`
+module.exports = p => `<div><div data-js="logoWrap">${require('./lib/logo')}</div><div>Finding the closest parking spots...sit tight.</div>`
